f1436395980d21a2dc93271d24b799434a3bbd08
Gabriel447 <Gabriel_447@live.com>
2022-10-20 20:58:19 -0300

    phpspreadsheet

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
 
-//testar tempo de execução da página
-
-$start = microtime(true);
-
-sleep(3);
-
-$end = microtime(true);
-
-echo 'O tempo que demorou para o meu script ser executado foi: ' . round(($start - $end), 2);
+// https://phpspreadsheet.readthedocs.io/en/latest/
---
+++ microtime.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// testar tempo de execução da página
+
+$start = microtime(true);
+sleep(3);
+$end = microtime(true);
+echo 'O tempo que demorou para o meu script ser executado foi: ' . round(($start - $end), 2);
